75123e52f07798de7a42649f586acbeaed6756c3
unknown <zahedivafa7@gmail.com>
2020-05-12 17:32:56 +0430

    Updates

---
+++ precache-manifest.9a3672b92adf7c182909b6cf873377d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8b3d962e2377d4a41605ae96356b7d",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "5465a807185aaa9d2849",
+    "url": "/ecommerce/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "e144c0d84f45096b2b48",
+    "url": "/ecommerce/static/css/main.9d03c1d5.chunk.css"
+  },
+  {
+    "revision": "5465a807185aaa9d2849",
+    "url": "/ecommerce/static/js/2.9164c2b3.chunk.js"
+  },
+  {
+    "revision": "3be7707146353ab2f9b2d7f9b9b67aea",
+    "url": "/ecommerce/static/js/2.9164c2b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e144c0d84f45096b2b48",
+    "url": "/ecommerce/static/js/main.c0276853.chunk.js"
+  },
+  {
+    "revision": "56c1273845e931b5493c",
+    "url": "/ecommerce/static/js/runtime-main.597e45e2.js"
+  },
+  {
+    "revision": "ccb88cac134072ba263cb18e0cbe8cd8",
+    "url": "/ecommerce/static/media/pattern.ccb88cac.png"
+  }
+]);
